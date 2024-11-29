049563a2203f9669da3588606fc30e3ded716cfe
Sarath Babu A S <sarath.s5@olakrutrim.com>
2024-11-29 14:16:29 +0530

    Latest_latest

--- drivers/add.py
+++ drivers/add.py
@@ -1,3 +1,7 @@
 def add(a, b):
     # Function to add two numbers.
     return a + b
+
+def sub(a, b):
+    # Function to subtract two numbers.
+    return a - b
--- tests/test_add.py
+++ tests/test_add.py
@@ -1,4 +1,4 @@
-from drivers.add import add
+from drivers.add import add , sub
 
 def test_positive_numbers():
     """Test addition of two positive numbers."""
@@ -11,3 +11,7 @@ def test_negative_numbers():
     assert result == -5, f"Expected -5 but got {result}"
 
 
+def test_sub_numbers():
+    """Test addition of two positive numbers."""
+    result = sub(2, 3)
+    assert result == -1, f"Expected -1 but got {result}"
